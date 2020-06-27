0f8f416d9fdfafac5825fa9145dd3a21ca770340
atul19802164 <44753404+atul19802164@users.noreply.github.com>
2020-06-27 21:39:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/classify_image', methods=['GET', 'POST'])
-def classify_image():
-    image_data = request.form['image_data']
-
-    response = jsonify(util.classify_image(image_data))
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run()
