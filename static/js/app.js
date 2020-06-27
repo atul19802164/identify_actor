5a48ae214fe26cc0d5e4233167915f73a4cfac98
atul19802164 <44753404+atul19802164@users.noreply.github.com>
2020-06-27 21:41:08 +0530

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+	var actorName = document.getElementById("actorName");
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+       
+        var url = "http://127.0.0.1:5000/classify_image";
+	
+        $.post(url, {
+            image_data: file.dataURL
+        },function(data, status) {
+			if(data.length>0){
+				 $("#actorName").show();
+			}
+    $("#actorName").html("<h2>" + data[0].class.toString()+"</h2>");                  
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    $("#actorName").hide();
+    init();
+});
